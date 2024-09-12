624a106b35510102030c669cb864979a0228dc72
user <mail@mail>
2024-09-12 11:53:18 +0900

    skeleton file for data generation

---
+++ data_generation/gen_random_data.py
@@ -0,0 +1,27 @@
+import os.path
+
+
+
+
+
+
+
+
+
+def write_output_without_overwrite(data: str):
+    appendix = 0
+    fname_base = "output/jobset_"
+    while True:
+        cfname = fname_base + str(appendix) + ".json"
+        if os.path.isfile(cfname):
+            appendix += 1
+            continue
+        else:
+            with open(cfname, "a") as f:
+                f.write(data)
+            break
+
+
+
+
+write_output_without_overwrite("test1")
--- solvers_heuristics/heuristics/main.py
+++ solvers_heuristics/heuristics/main.py
@@ -13,8 +13,13 @@ Skills = IntEnum('Skills', ['C_Plus_Plus', 'C_Sharp', 'Python', 'Ruby', 'Office'
 # single_job = {'skill_required': Skills.C_Sharp,
 #               'skill_level_required': 5,
 #               'base_duration': 20,
-#               'deadline': 40}
+#               'deadline': 40,
+#               'index': 0,
+#               'name': "performance benchmarking",
+#              }
 jobs = []
+with open('../../data_generation/outputs/jobset_1.json', 'r') as f:
+    jobs_dict = json.load(f)
 # add this urself
 seminars = []
 
