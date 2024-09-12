f69ebc9ba95afff790b31dcdc5bae56d12b9c46d
user <mail@mail>
2024-09-12 11:11:33 +0900

    added some heuristics main.py logic

--- solvers_heuristics/heuristics/main.py
+++ solvers_heuristics/heuristics/main.py
@@ -1,1 +1,26 @@
+import json
+from enum import IntEnum
+
+with open("../../resources/config.json", "r") as f:
+    config_dict = json.load(f)
+
+
+number_of_machines = 3
+machines_array = [[] for _ in range(number_of_machines)]
+Skills = IntEnum('Skills', ['C_Plus_Plus', 'C_Sharp', 'Python', 'Ruby', 'Office', 'Communication'])
+
+# this should come from a file or generator function and contain tuples like this
+# single_job = {'skill_required': Skills.C_Sharp,
+#               'skill_level_required': 5,
+#               'base_duration': 20,
+#               'deadline': 40}
+jobs = []
+# add this urself
+seminars = []
+
+jobs += seminars
+
+current_time = 0
+
+
 
