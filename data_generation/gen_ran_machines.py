d3a6ecc4eb1921bdf2f4b829c50dfd74b48d11c1
Erik Hersmann <e.hersmann@gmail.com>
2024-09-13 14:45:53 +0900

    the basic part of caculating makespan and greedily planning the jobs without seminars is working now

--- data_generation/gen_ran_machines.py
+++ data_generation/gen_ran_machines.py
@@ -1,6 +1,25 @@
 import json
+from random import randint
 
-with open("../../resources/config.json", "r") as f:
+with open("../resources/config.json", "r") as f:
     config_dict = json.load(f)
 
 
+if __name__ == "__main__":
+    machines = []
+    for idx in range(config_dict["number_of_machines"]):
+        machines.append(
+            {
+                "name": "employee #" + str(idx+1),
+                "index": idx,
+                "skills": [
+                    randint(
+                        config_dict["skill_config"]["min_machine_skill"],
+                        config_dict["skill_config"]["max_machine_skill"],
+                    )
+                    for _ in range(len(config_dict["skills"]))
+                ],
+            }
+        )
+    with open("output/machineset_0.json", "w") as f:
+        json.dump(machines, f)
--- solvers_heuristics/heuristics/main.py
+++ solvers_heuristics/heuristics/main.py
@@ -1,13 +1,36 @@
 import json
 from math import ceil
 
+
+def dprint(dictionary_list, name):
+    print(f"{name}\n")
+    for row in dictionary_list:
+        print(row)
+    print()
+
+
+def sort_by_priority(jobs_list, mode=0):
+    match mode:
+        case 0:
+            return enumerate(jobs_list)
+        case 1:
+            return sorted(enumerate(jobs_list), key=lambda x: x[1]["deadline"])
+        
+
+def calculate_lateness(job_order_on_machines):
+    lateness = 0
+    for machine in job_order_on_machines:
+        for job in machine:
+            lateness += job['end_time'] - job['deadline']
+    return lateness
+
+
 with open("../../resources/config.json", "r") as f:
     config_dict = json.load(f)
 
 
 number_of_machines = config_dict["number_of_machines"]
 # must be a list of dicts
-machine_qualifications = []
 job_order_on_machines = [[] for _ in range(number_of_machines)]
 
 
@@ -21,34 +44,92 @@ job_order_on_machines = [[] for _ in range(number_of_machines)]
 #              }
 with open("../../data_generation/output/jobset_0.json", "r") as f:
     jobs = json.load(f)
-print(jobs[0])
 # add this urself
 with open("../../data_generation/output/seminarset_basic_0.json", "r") as f:
     seminars = json.load(f)
+with open("../../data_generation/output/machineset_0.json", "r") as f:
+    machine_qualifications = json.load(f)
 
 
-print(jobs)
-print(seminars)
+dprint(jobs, "Jobs")
+dprint(seminars, "Seminars")
+dprint(machine_qualifications, "Machines")
 
 
 current_time = 0
 while len(jobs) > 0:
     # Calculate times list for all jobs for all free machines
     all_current_machine_processing_times = []
-    currently_free_machines = [machine[-1] for machine in job_order_on_machines if machine[-1]['end_time'] < current_time]
+    # Get the last job on all machines whose finish time (on that job) is less than the current time
+    currently_free_machines = [
+        idx
+        for idx, machine in enumerate(job_order_on_machines)
+        if len(machine) == 0 or machine[-1]["end_time"] < current_time
+    ]
+    print(f"t={current_time}\tFree machines\t{currently_free_machines}\n")
     # Before Seminars
     for machine in currently_free_machines:
         current_machine_processing_times = []
         for job in jobs:
+            # Calculate the new processing time (Maybe use a function here to allow for changing this later however this shouldn't be too much of an issue 問題ない)
             current_machine_processing_times.append(
-                ceil(job["basetime"] / machine_qualifications[job["skill_index"]])
+                ceil(
+                    job["base_duration"]
+                    * (
+                        job["skill_level_required"]
+                        / machine_qualifications[machine]["skills"][
+                            job["skill_required"]
+                        ]
+                    )
+                )
             )
-        all_current_machine_processing_times.append(current_machine_processing_times)
+        all_current_machine_processing_times.append(
+            (machine, current_machine_processing_times)
+        )
+    # print(all_current_machine_processing_times)
     # Recalculate with seminars
-    
+
     # Decide whether to consider seminars ?
     # For each seminar consider the processing times again and if they get less lateness overall plan a seminar
 
-
     # If a seminar hasnt been chosen order each machines job processing times by relative speed to baseline
-    
+
+    # Greedily pick the earliest deadline jobs and the fastest worker on them ?
+    print(f"current machine processing time {all_current_machine_processing_times}\n")
+    machines_used_in_this_iteration = []
+    for job_index, job in sort_by_priority(jobs, 1):
+        # find the machine that has the shortest processing time for the current job
+        if len(machines_used_in_this_iteration) == len(
+            all_current_machine_processing_times
+        ):
+            break
+        best_machine_idx = sorted(
+            [
+                machine
+                for machine in all_current_machine_processing_times
+                if machine[0] not in machines_used_in_this_iteration
+            ],
+            key=lambda x: x[1][job_index],
+            reverse=True,
+        )[-1][0]
+        # plan the job on that machine and dont consider it for the next few jobs
+        job_order_on_machines[best_machine_idx].append(job)
+        job_order_on_machines[best_machine_idx][-1]["start_time"] = current_time
+        job_order_on_machines[best_machine_idx][-1]["end_time"] = (
+            current_time
+            + [
+                processing_time
+                for processing_time in all_current_machine_processing_times
+                if processing_time[0] == best_machine_idx
+            ][0][1][job_index]
+        )
+
+        jobs.remove(job)
+        machines_used_in_this_iteration.append(best_machine_idx)
+        # break
+
+    dprint(job_order_on_machines, "Order")
+    current_time = min([job_on_machine[-1]['end_time'] for job_on_machine in job_order_on_machines]) + 1
+    # t = min([job_on_machine[-1][]])
+
+print(calculate_lateness(job_order_on_machines))
