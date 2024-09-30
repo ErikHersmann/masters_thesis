548e9a2453f3909e2cc5417b940394ad8b3ee1af
Erik Hersmann <e.hersmann@gmail.com>
2024-09-30 14:43:27 +0900

    working calculate_lateness function, not too sure

--- solvers_heuristics/heuristics/calculate_lateness.py
+++ solvers_heuristics/heuristics/calculate_lateness.py
@@ -14,20 +14,30 @@ def calculate_lateness(
     """
     machine_countdowns = [0 for _ in range(len(machines))]
     current_time = 0
-    for machine_idx, countdown in enumerate(machine_countdowns):
-        if countdown > 0:
-            continue
-        current_job = order_on_machines[machine_idx].pop(0)
-        # Calculate the processing duration here (if its a non-seminar job)
-        if current_job["type"] != "seminar":
-            current_processing_duration = current_job["baseline_duration"] * (
-                current_job["required_skill_level"]
-                / machines[machine_idx]["skills"][current_job["required_skill"]]
+    end = False
+    while not end:
+        if sum([len(order) for order in order_on_machines]) == 0:
+            break
+        for machine_idx, countdown in enumerate(machine_countdowns):
+            if countdown > 0:
+                continue
+            current_job = order_on_machines[machine_idx].pop(0)
+            # Calculate the processing duration here (if its a non-seminar job)
+            if current_job["type"] != "seminar":
+                current_processing_duration = current_job["baseline_duration"] * (
+                    current_job["required_skill_level"]
+                    / machines[machine_idx]["skills"][current_job["required_skill"]]
+                )
+            else:
+                current_processing_duration = current_job["baseline_duration"]
+            machine_countdowns[machine_idx] = current_processing_duration
+            # Update the proficiency of that machine here
+            machines[machine_idx]["skills"][current_job["required_skill"]] = (
+                machines[machine_idx]["beta"]
+                + machines[machine_idx]["alpha"]
+                * machines[machine_idx]["skills"][current_job["required_skill"]]
             )
-        else:
-            current_processing_duration = current_job["baseline_duration"]
-        machine_countdowns[machine_idx] = current_processing_duration
-        # Update the proficiency of that machine here
+        current_time += min([ctdwn for ctdwn in machine_countdowns if ctdwn > 0])
         
         
-    return 0
+    return current_time
