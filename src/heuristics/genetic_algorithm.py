5e244ded7b4b22865e7b6841d125eb4f4f3446c3
Erik Hersmann <e.hersmann@gmail.com>
2024-10-08 20:56:08 +0900

    refactored code in heuristics a little bit

--- src/heuristics/genetic_algorithm.py
+++ src/heuristics/genetic_algorithm.py
@@ -1,8 +1,7 @@
 import json
-from calculate_lateness import calculate_lateness
-from pprint import pprint
 from random import randint, choice, shuffle, seed
 from math import floor
+from algorithm_template import heuristic_template
 
 
 # This should all work without knowledge of jobs/seminars/machines
@@ -10,31 +9,23 @@ from math import floor
 seed(7)
 
 
-class genetic_algorithm:
+class genetic_algorithm(heuristic_template):
     """Recombine Parent generation\\
     Mutate their children\\
     Repair the children\\
     Selection -> next parent generation
     """
 
-    def __init__(self, N_JOBS, N_SEMINARS, N_MACHINES) -> None:
-        with open("../../resources/config.json", "r") as f:
-            self.config = json.load(f)
-
-        self.N_JOBS = N_JOBS
-        self.N_SEMINARS = N_SEMINARS
-        self.N_MACHINES = N_MACHINES
-        self.current_generation = []
+    def __init__(self, machines, jobs_seminars) -> None:
+        heuristic_template.__init__(self, machines, jobs_seminars)
         self.N_PARENTS = 6  # 5 is naturally repeating (5 parents generate 10 children, which become 5 parents after 1 round of selection)
-        self.current_epoch = 1
-        self.mutation_probability = 0.05
-        self.mut_treshold = 100 - floor(100 * self.mutation_probability)
-        self.lateness_calculator = None
-        self.MAX_POPULATION_SIZE = 50
-        self.best = [1000, None]
+        self.MAX_POP_SIZE = 50
+        self.F_MUT_PROB = 0.05
+        self._current_epoch = 1
+        self._mut_treshold = 100 - floor(100 * self.F_MUT_PROB)
+        self._best = [1000, None]
+        self._current_generation = []
         self.generate_first_generation()
-        
-    
 
     def generate_first_generation(self):
         """Used to generate a semi random starting generation"""
@@ -56,41 +47,41 @@ class genetic_algorithm:
                 for job_index in range(self.N_JOBS):
                     if all_jobs[job_index] not in jobs_picked:
                         cur_solution[-1].append(all_jobs[job_index])
-            self.current_generation.append(cur_solution)
+            self._current_generation.append(cur_solution)
         self.repair()
 
     def mutate(self):
         """Random mutation via shuffling machine indices or job indices on a machine\\
         probability can be set via the class parameter
         """
-        for candidate_index, candidate in enumerate(self.current_generation):
-            if randint(1, 100) > self.mut_treshold:
-                shuffle(self.current_generation[candidate_index])
+        for candidate_index, candidate in enumerate(self._current_generation):
+            if randint(1, 100) > self._mut_treshold:
+                shuffle(self._current_generation[candidate_index])
             for machine_index in range(len(candidate)):
-                if randint(1, 100) > self.mut_treshold:
-                    shuffle(self.current_generation[candidate_index][machine_index])
+                if randint(1, 100) > self._mut_treshold:
+                    shuffle(self._current_generation[candidate_index][machine_index])
         self.repair()
 
     def repair(self):
         """Supposed to turn all solutions into valid ones\\
         Might be buggy still
         """
-        for candidate_index, candidate in enumerate(self.current_generation):
+        for candidate_index, candidate in enumerate(self._current_generation):
             current_jobs = []
             for machine_index in range(len(candidate)):
                 current_seminars = []
-                self.current_generation[candidate_index][machine_index] = list(
+                self._current_generation[candidate_index][machine_index] = list(
                     dict.fromkeys(
-                        self.current_generation[candidate_index][machine_index]
+                        self._current_generation[candidate_index][machine_index]
                     )
                 )
                 for job_index, job in enumerate(
-                    self.current_generation[candidate_index][machine_index]
+                    self._current_generation[candidate_index][machine_index]
                 ):
                     if (job < self.N_JOBS and job in current_jobs) or (
                         job >= self.N_JOBS and job in current_seminars
                     ):
-                        self.current_generation[candidate_index][machine_index].pop(
+                        self._current_generation[candidate_index][machine_index].pop(
                             job_index
                         )
                     else:
@@ -99,36 +90,36 @@ class genetic_algorithm:
 
             for job in range(self.N_JOBS):
                 if job not in current_jobs:
-                    self.current_generation[candidate_index][-1].append(job)
+                    self._current_generation[candidate_index][-1].append(job)
 
     def selection(self):
         """Halfs the current population count via random comparison of 2 individuals and discarding the less fit one\\
         Maybe keep the top k ones always regardless of of win or lose
         """
         while True:
-            shuffle(self.current_generation)
+            shuffle(self._current_generation)
             next_generation = []
-            for idx in range(0, len(self.current_generation), 2):
-                if idx == len(self.current_generation) - 1:
+            for idx in range(0, len(self._current_generation), 2):
+                if idx == len(self._current_generation) - 1:
                     fitness2 = 10000
                 else:
                     fitness2 = self.lateness_calculator.calculate(
-                        self.current_generation[idx + 1]
+                        self._current_generation[idx + 1]
                     )
                 fitness1 = self.lateness_calculator.calculate(
-                    self.current_generation[idx]
+                    self._current_generation[idx]
                 )
                 # print(fitness1, fitness2)
                 if fitness1 <= fitness2:
-                    next_generation.append(self.current_generation[idx])
-                    if fitness1 < self.best[0]:
-                        self.best = [fitness1, self.current_generation[idx]]
+                    next_generation.append(self._current_generation[idx])
+                    if fitness1 < self._best[0]:
+                        self._best = [fitness1, self._current_generation[idx]]
                 else:
-                    next_generation.append(self.current_generation[idx + 1])
-                    if fitness2 < self.best[0]:
-                        self.best = [fitness2, self.current_generation[idx]]
-            self.current_generation = next_generation
-            if len(self.current_generation) <= self.MAX_POPULATION_SIZE:
+                    next_generation.append(self._current_generation[idx + 1])
+                    if fitness2 < self._best[0]:
+                        self._best = [fitness2, self._current_generation[idx]]
+            self._current_generation = next_generation
+            if len(self._current_generation) <= self.MAX_POP_SIZE:
                 break
 
     def recombination(self):
@@ -136,30 +127,30 @@ class genetic_algorithm:
         Take all entries up to the random index from parent 1's machine and all the entries after the index from parent 2's machine
         """
         children = []
-        for parent1_idx in range(len(self.current_generation)):
-            for parent2_idx in range(parent1_idx + 1, len(self.current_generation)):
+        for parent1_idx in range(len(self._current_generation)):
+            for parent2_idx in range(parent1_idx + 1, len(self._current_generation)):
                 children.append([])
                 machine_idx = 0
                 for m1, m2 in zip(
-                    self.current_generation[parent1_idx],
-                    self.current_generation[parent2_idx],
+                    self._current_generation[parent1_idx],
+                    self._current_generation[parent2_idx],
                 ):
                     split_point = randint(0, min(len(m1), len(m2)))
                     children[-1].append(
-                        self.current_generation[parent1_idx][machine_idx][
+                        self._current_generation[parent1_idx][machine_idx][
                             : split_point + 1
                         ]
                     )
                     children[-1][-1].extend(
-                        self.current_generation[parent2_idx][machine_idx][split_point:]
+                        self._current_generation[parent2_idx][machine_idx][split_point:]
                     )
                     machine_idx += 1
-        self.current_generation = children
-        self.current_epoch += 1
+        self._current_generation = children
+        self._current_epoch += 1
         self.mutate()
 
     def __str__(self):
-        return "\n".join([str(x) for x in self.current_generation])
+        return "\n".join([str(x) for x in self._current_generation])
 
 
 def setup():
@@ -182,10 +173,8 @@ def setup():
 
 
 if __name__ == "__main__":
-    algo = genetic_algorithm(5, 2, 3)
-    # Rewrite this to use a method for setting up the calculator and reading in the problem 
-    algo.lateness_calculator = calculate_lateness(*setup())
-    while algo.current_epoch < 20:
+    algo = genetic_algorithm(*setup())
+    while algo._current_epoch < 20:
         algo.recombination()
         algo.selection()
-    print(algo.best)
+    print(algo._best)
---
+++ src/heuristics/simulated_annealing.py
@@ -0,0 +1,9 @@
+
+
+
+class simulated_annealing():
+    """Not sure how this works I'll consult the papers
+    """    
+    def __init__(self) -> None:
+        pass
+    
